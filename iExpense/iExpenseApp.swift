2f891e376e7ab77cb2fbec717b8da7934f560a3e
midkids <83714759+midkids@users.noreply.github.com>
2026-04-27 21:09:45 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Myron Snelson on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
