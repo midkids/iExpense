9ea55a60556b569aa013d2b504eca1d4381fbf80
midkids <83714759+midkids@users.noreply.github.com>
2026-05-03 22:03:02 -0400

    Added app storage to persistent data such as user defaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -26,6 +26,9 @@ import SwiftUI
 //   and any views that read that property
 //   will be updated
 /*
+// Using @State with classes
+// Sharing SwiftUI state with @Observable
+//
 @Observable
 class User {
     var firstName = "Bilbo"
@@ -71,6 +74,8 @@ struct ContentView: View {
  */
 
 /*
+// Showing and hiding views
+//
 // Will show this second view via sheet
 // Can drag this view away
 struct SecondView: View {
@@ -99,6 +104,9 @@ struct ContentView: View {
 }
  */
 
+/*
+// Deleting items using onDelete()
+//
 struct ContentView: View {
     @State private var numbers = [Int]()
     @State private var currentNumber = 1
@@ -128,6 +136,52 @@ struct ContentView: View {
     
     func removeRows(at offsets: IndexSet) {
         numbers.remove(atOffsets: offsets)
+    }
+}
+ */
+
+// The simplest way to read and write a small of data
+// is through user defaults
+// It is a great way to keep user preferences
+// Storing too many user defaults will slow the
+// loading of your app
+// Should store no more than 512 KB
+
+struct ContentView: View {
+    
+//    @State private var tapCount = 0
+    
+    // If the key cannot be found (as in the first time
+    // the app runs), the value of Tap defaults to 0
+    // because it is an integer
+    // Booleans default to false
+    // iOS buffers writing default values and
+    // it can take a few seconds to write all the user defaults
+    // However, in theory, nothing will ever be lost
+//    @State private var tapCount =
+//     UserDefaults.standard.integer(forKey: "Tap")
+    
+    // IMPORTANT: App storage is a MUCH better choice
+    // than user defaults (user defaults actually works
+    // by using app storage)
+    // When the value of tapCount changes,
+    //   SwiftUI will reinvoke the body property automatically
+    //   This makes sure the user interface always
+    //   reflects the latest value
+    // The default value is used if there is no value set
+    @AppStorage("tapCount") private var tapCount = 0
+    
+    var body: some View {
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+            // Standard is the built in instance of User Defaults
+            // that is attached to our apps (can create your own)
+            // Single set method and
+            // It can store integers, strings, booleans
+            // Must give a string name (e.g. "Tap")
+            // to the value we are writing
+            UserDefaults.standard.set(tapCount, forKey: "Tap")
+        }
     }
 }
 
